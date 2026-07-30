680065e1f0504171ec1146ec6301ad603d0b65a3
Durga Chiranjeevi <226m1a4263@gmail.com>
2026-07-30 13:02:29 +0530

    Added two more lones

--- track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/WelcomeToJava.java
+++ track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/WelcomeToJava.java
@@ -4,5 +4,7 @@ public class WelcomeToJava {
         System.out.println("Welcome to java track");
         System.out.println("I already know how to build programming logic");
         System.out.println("Now iam learning how to express the logic using java");
+        System.out.println("java is a oop language");
+        System.out.println("it is platform independent language");
     }
 }
