c59aa23336c1997c9cb8ab1f3c0c6877a212d301
Durga Chiranjeevi <226m1a4263@gmail.com>
2026-07-30 12:45:14 +0530

    Day 1 changes

--- track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/WelcomeToJava.java
+++ track/M01-understanding-java/T01-programming-languages-and-java/M01-T01-ST01-L-welcome-to-java-track/work/src/WelcomeToJava.java
@@ -1,5 +1,8 @@
 public class WelcomeToJava {
     public static void main(String[] args) {
-       //Write your code here
+        // Write your code here
+        System.out.println("Welcome to java track");
+        System.out.println("I already know how to build programming logic");
+        System.out.println("Now iam learning how to express the logic using java");
     }
 }
