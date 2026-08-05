cd7f2684d19ca44b47b0b75c787790d9b3c12ad0
Durga Chiranjeevi <226m1a4263@gmail.com>
2026-08-05 12:20:43 +0530

    Example for FOR LOOP

--- track/M02-Introduction-to-java/src/Switchex.java
+++ track/M02-Introduction-to-java/src/Switchex.java
@@ -4,27 +4,27 @@ public class Switchex {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter day 1 - 7");
-        String day = sc.next();
+        int day = sc.nextInt();
         switch (day) {
-            case "1":
+            case 1:
                 System.out.println("Monday");
                 break;
-            case "2":
+            case 2:
                 System.out.println("Tuesday");
                 break;
-            case "3":
+            case 3:
                 System.out.println("Wednesday");
                 break;
-            case "4":
+            case 4:
                 System.out.println("Thursday");
                 break;
-            case "5":
+            case 5:
                 System.out.println("Friday");
                 break;
-            case "6":
+            case 6:
                 System.out.println("Saturday");
                 break;
-            case "7":
+            case 7:
                 System.out.println("Sunday");
                 break;
             default:
