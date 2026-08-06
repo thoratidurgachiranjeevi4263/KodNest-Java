be86d12fe75b10a9ae2be73eb5713d054292b02e
Durga Chiranjeevi <226m1a4263@gmail.com>
2026-08-06 11:37:28 +0530

    Example for Nested While Loop

---
+++ track/M02-Introduction-to-java/src/NestedWhileLoop.java
@@ -0,0 +1,14 @@
+public class NestedWhileLoop {
+    public static void main(String[] args) {
+        int i = 1;
+        while (i <= 5) {
+            int j = 1;
+            while (j <= 5) {
+                System.out.println(j);
+                j++;
+            }
+            i++;
+            System.out.println();
+        }
+    }
+}
